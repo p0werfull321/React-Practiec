42216efdb9034de608dc5e8026daa9133da19373
param <marap>
2024-06-18 22:39:50 +0530

    CRUD function

--- src/CRUD.js
+++ src/CRUD.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
+import "./CRUD.css";
 
 const arr = [
   { id: 1, first: "ram", last: "singh", age: 15 },
@@ -10,8 +11,11 @@ const arr = [
 ];
 
 const CRUD = () => {
-  const [data, setData] = useState();
   const [data1, setData1] = useState([]);
+  const [first, setfirst] = useState();
+  const [last, setLast] = useState();
+  const [age, setAge] = useState();
+  const [id, setId] = useState();
 
   useEffect(() => {
     setData1(arr);
@@ -24,8 +28,68 @@ const CRUD = () => {
     setData1(a);
   }
 
+  function EditRow(index) {
+    let a = data1.filter(function (a, Curr_index) {
+      return index === Curr_index;
+    });
+    setfirst(a[0].first);
+    setLast(a[0].last);
+    setAge(a[0].age);
+    setId(a[0].id);
+  }
+
+  function updateIndex() {
+    let a = data1
+      .map(function (val) {
+        return val.id;
+      })
+      .indexOf(id);
+    let x = [...data1];
+    x[a].first = first;
+    x[a].last = last;
+    x[a].age = age;
+    setData1(x);
+  }
+
+  function saveItems() {
+    let obj = { id: data1.length + 1, first: first, last: last, age: age };
+    let x = [...data1];
+    x.push(obj);
+    setData1(x);
+  }
+
   return (
     <div>
+      <div class="a">
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={first}
+            onChange={(a) => setfirst(a.target.value)}
+          />
+        </label>
+        <label>
+          Last Name:{" "}
+          <input
+            type="text"
+            value={last}
+            onChange={(a) => setLast(a.target.value)}
+          />
+        </label>
+        <label>
+          Age:{" "}
+          <input
+            type="text"
+            value={age}
+            onChange={(a) => setAge(a.target.value)}
+          />
+        </label>
+        <button onClick={updateIndex}>Update</button>
+        <button onClick={saveItems}>Save</button>
+        <br />
+        <br />
+      </div>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -45,7 +109,12 @@ const CRUD = () => {
                 <td>{a.last}</td>
                 <td>{a.age}</td>
                 <td>
-                  <Button className="btn btn-primary mr-3">Edit</Button>
+                  <Button
+                    onClick={() => EditRow(index)}
+                    className="btn btn-primary mr-3"
+                  >
+                    Edit
+                  </Button>
                   <Button
                     onClick={() => removeRow(index)}
                     className="btn btn-danger ml-3"
